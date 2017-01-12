869500e2577045c2d884f01b37eb59117cd30f04
Karim-Pierre Maalej <kpm@kypselia.com>
2017-01-12 11:31:31 +0100

    Implemented basic List View Controller

--- MyVelib/ListViewController.swift
+++ MyVelib/ListViewController.swift
@@ -12,6 +12,10 @@ class ListViewController: UIViewController {
   
   var index: Int = 0
   var list: StationsList! 
+  private let listDataSource = ListDataSource()
+  
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var tableView: UITableView!
   
   class func instantiate() -> ListViewController {
     let storyboardIdentifier = "List"
@@ -21,20 +25,37 @@ class ListViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    listDataSource.list = list
+    tableView.dataSource = listDataSource
   }
   
   override func viewDidAppear(_ animated: Bool) {
     super.viewDidAppear(animated)
     
-    let type = list is FavoriteStationsList ? "Favoris" : "Géoloc"
-    print("page #\(index) : \(type)")
-    
     refreshUI()
   }
   
   func refreshUI() {
+    titleLabel.text = list is FavoriteStationsList ? "Favoris" : "Géoloc"
+    tableView.reloadData()
     print("Stations count: \(list.allStations.count)")
     print("Stations in short list: \(list.shortStationsList.count)")
   }
   
+}
+
+class ListDataSource: NSObject, UITableViewDataSource {
+  var list: StationsList!
+  
+  public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return list.shortStationsList.count
+  }
+  
+  public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let station = list.shortStationsList[indexPath.row]
+    let cell = tableView.dequeueReusableCell(withIdentifier: "defaultCell", for: indexPath)
+    cell.textLabel?.text = station.name
+    return cell
+  }
+
 }
