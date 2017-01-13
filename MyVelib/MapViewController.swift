898db3b1f888f366af5071cf7731cc00cd7c96cc
Karim-Pierre Maalej <kpm@kypselia.com>
2017-01-13 12:55:18 +0100

    Added basic map to MapViewController

--- MyVelib/MapViewController.swift
+++ MyVelib/MapViewController.swift
@@ -7,14 +7,28 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
   
   var list: StationsList! 
   var stationToShow: Station?
 
+  @IBOutlet weak var mapView: MKMapView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    mapView.addAnnotations(list.allStations.map(StationAnnotation.init))
+    
+    if let stationToShow = stationToShow {
+      centerMap(at: stationToShow.position.coordinate)
+      let selectedAnnotation = mapView.annotations.flatMap { $0 as? StationAnnotation }.filter { $0.station.number == stationToShow.number }.first!
+      mapView.selectAnnotation(selectedAnnotation, animated: false)
+    } else if list.shortStationsList.count > 0 {
+      centerMap(at: list.shortStationsList[0].position.coordinate)
+    } else {
+      centerMap(at: list.allStations[0].position.coordinate)
+    }
   }
   
   override func viewDidAppear(_ animated: Bool) {
@@ -24,6 +38,29 @@ class MapViewController: UIViewController {
   
   @IBAction func close() {
     dismiss(animated: true, completion: nil)
+  }
+
+  func centerMap(at coordinate: CLLocationCoordinate2D) {
+    let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+    let region = MKCoordinateRegion(center: coordinate, span: span)
+    mapView.setRegion(region, animated: false)
+  }
+}
+
+class StationAnnotation: NSObject, MKAnnotation {
+  let station: Station
+  
+  init(station: Station) {
+    self.station = station
+    super.init()
+  }
+  
+  var coordinate: CLLocationCoordinate2D { 
+    return station.position.coordinate
+  }
+  
+  var title: String? { 
+    return station.name
   }
 
 }
